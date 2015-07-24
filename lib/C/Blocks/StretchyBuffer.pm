f9316fad901ccc9bb731031247ee2c3c4bf8d2a7
David Mertens <dcmertens.perl@gmail.com>
2015-07-24 10:55:40 -0400

    Switch StretchyBuffer to libperltemp to prevent segfaults

--- lib/C/Blocks/StretchyBuffer.pm
+++ lib/C/Blocks/StretchyBuffer.pm
@@ -3,6 +3,7 @@ use warnings;
 package C::Blocks::StretchyBuffer;
 use C::Blocks;
 use C::Blocks::libperltemp;  # for croak and memory stuff
+#use C::Blocks::libperl;  # for croak and memory stuff
 
 cshare {
 	#define sbfree(a)         ((a) ? Safefree(stb__sbraw(a)),0 : 0)
