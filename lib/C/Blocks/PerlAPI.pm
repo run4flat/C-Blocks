2c2fc8c66a60b07e3deb979a29318efd1e2ca07c
David Mertens <dcmertens.perl@gmail.com>
2016-06-02 22:26:48 -0400

    Version bump for next round of dev work

--- lib/C/Blocks/PerlAPI.pm
+++ lib/C/Blocks/PerlAPI.pm
@@ -16,7 +16,7 @@ BEGIN {
 require DynaLoader;
 our @ISA = qw( DynaLoader C::Blocks::libloader );
 
-our $VERSION = '0.05';
+our $VERSION = '0.05_01';
 bootstrap C::Blocks::PerlAPI $VERSION;
 $VERSION = eval $VERSION;
 
