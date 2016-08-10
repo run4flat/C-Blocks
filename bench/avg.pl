68c63237bfedc6a38b7a4667c716c39f3e012aa9
David Mertens <dcmertens.perl@gmail.com>
2016-08-09 22:35:09 -0400

    Add Perl implementation to avg benchmark

--- bench/avg.pl
+++ bench/avg.pl
@@ -7,22 +7,28 @@ use PDL;
 use Benchmark qw(timethese :hireswallclock);
 
 # Generate some data
-my ($pdl_data, $packed_data, $N);
+my (@data, $pdl_data, $packed_data, $N);
 for my $log_n (1, 2, 3, 4, 5, 6, 7) {
 	$N = 10**$log_n;
 	print "--- For N = $N ---\n";
 	
-	my @data = map { rand() } 1 .. $N;
+	@data = map { rand() } 1 .. $N;
 	$pdl_data = pdl(\@data);
 	$packed_data = pack('d*', @data);
 	
-	timethese(1000, {  PDL => \&pdl_avg, CBlocks => \&c_blocks_avg});
+	timethese(1000, {  PDL => \&pdl_avg, CBlocks => \&c_blocks_avg, Perl => \&perl_avg});
 	print "PDL returned ", pdl_avg(), " and c_blocks_avg returned ",
 		c_blocks_avg(), "\n";
 }
 
 sub pdl_avg {
 	return $pdl_data->avg;
+}
+
+sub perl_avg {
+	my $sum = 0;
+	$sum += $_ foreach @data;
+	return $sum / @data;
 }
 
 sub c_blocks_avg {
