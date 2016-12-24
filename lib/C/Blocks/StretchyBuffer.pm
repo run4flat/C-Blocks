10d35b149b0e24ef7e40ab100dd5d393a74045fc
David Mertens <dcmertens.perl@gmail.com>
2016-12-24 13:28:48 -0500

    Update C::Blocks::StretchyBuffer

--- lib/C/Blocks/StretchyBuffer.pm
+++ lib/C/Blocks/StretchyBuffer.pm
@@ -8,7 +8,7 @@ $VERSION = eval $VERSION;
 no warnings qw(C::Blocks::compiler);
 
 cshare {
-	#define sbfree(a)         ((a) ? Safefree(stb__sbraw(a)),0 : 0)
+	#define sbfree(a)         ((a) ? Safefree(stb__sbraw(a)),a=0,0 : 0)
 	#define sbpush(a,v)       (stb__sbmaybegrow(a,1), (a)[stb__N_used(a)++] = (v))
 	#define sbpop(a)          ((a && stb__N_used(a)) ? (a)[--stb__N_used(a)] : 0)
 	#define sbcount(a)        ((a) ? stb__N_used(a) : 0)
@@ -54,6 +54,43 @@ cshare {
 	}
 }
 
+use C::Blocks::Types::Pointers generic_buffer => 'void *';
+use C::Blocks::Types qw(uint);
+sub sbcount {
+	my generic_buffer $buffer = shift;
+	my uint $count = 0;
+	cblock {
+		$count = sbcount($buffer);
+	}
+	return $count;
+}
+sub sbfree {
+	my generic_buffer $buffer = shift;
+	cblock {
+		sbfree($buffer);
+	}
+}
+
+sub sbremove {
+	my generic_buffer $buffer = shift;
+	my uint $N_to_remove = shift;
+	my uint $to_return = 0;
+	cblock {
+		$to_return = sbremove($buffer, $N_to_remove);
+	}
+	return $to_return;
+}
+
+use Exporter ();
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(sbcount sbfree sbremove);
+
+no warnings 'C::Blocks::import', 'redefine';
+sub import {
+	C::Blocks::load_lib(@_);
+	__PACKAGE__->export_to_level(1, @_);
+}
+
 1;
 
 __END__
@@ -65,64 +102,62 @@ C::Blocks::StretchyBuffer - Enabling stretchy buffers in your context
 =head1 SYNOPSIS
 
  use C::Blocks;
- use C::Blocks::StretchyBuffer;
+ use C::Blocks::StretchyBuffer qw(sbcount sbfree);
+ use C::Blocks::Types::Pointers dbuffer => 'double *';
  
  # Create a function that uses stretchy buffers:
- $context->code('Body') = C::TinyCompiler::line_number(__LINE__) . q{
-     void test_func() {
-         /* stretchy buffers always start and end as null pointers */
-         double * list = 0;
-         
-         /* Push some values onto the list */
-         sbpush(list, 3.2);
-         sbpush(list, -2.9);
-         sbpush(list, 5);
-         
-         /* Eh, let's change that last one */
-         list[2] = 22;
-         
-         /* Allocate room for five more */
-         sbadd(list, 5);
-         
-         /* Get the list length */
-         printf("List has %d available elements\n", sbcount(list));
-         
-          /* Set the last element */
-         sblast(list) = 100;
-         
-         /* Pop the last element */
-         printf("Last element was %f\n", sbpop(list));
-         
-         /* How many elements do we have? */
-         printf("After a pop, we have %d available elements\n", sbcount(list));
-         
-         /* Remove two elements */
-         int remaining = sbremove(list, 2);
-         printf("sbremove returned %d\n", remaining);
-         
-         printf("After remove, we have %d available elements\n", sbcount(list));
-         
-         int i;
-         printf("List:\n");
-         for (i = 0; i < sbcount(list); i++) {
-             printf("%d: %f\n", i, list[i]);
-         }
-         
-         /* When we're all done, free the memory, restoring the value to null */
-         sbfree(list);
-     }
- };
+ my dbuffer $list = 0;
+ cblock {
+     /* Push some values onto the list */
+     sbpush($list, 3.2);
+     sbpush($list, -2.9);
+     sbpush($list, 5);
+ }
  
- # Compile and call:
- $context->compile;
- $context->call_void_function('test_func');
+ print "I just pushed ", sbcount($list), " items onto my list\n";
+ 
+ cblock {
+     /* Eh, let's change that last one from 5 to 22 */
+     $list[2] = 22;
+     
+     /* run through the list */
+     for (int i = 0; i < sbcount($list); i++) {
+         printf("%dth item is %g\n", i, $list[i]);
+	 }
+	 
+	 /* remove last item */
+	 double removed = sbpop($list);
+	 printf("Just removed %g; now list has %d items\n",
+         removed, sbcount($list));
+     
+     /* Allocate room for five more */
+     sbadd($list, 5);
+     
+     /* Set the last element */
+     sblast($list) = 100;
+     
+     /* Remove two elements */
+     int N_remaining = sbremove($list, 2);
+     
+     /* When we're all done, free the memory, restoring the value to null */
+     sbfree($list);
+     
+     printf("list is at %p\n", $list);
+ }
+ 
+ # It's ok if we ask for the length of a null value:
+ print "After freeing, length is ", sbcount($list), "\n";
+ 
+ # Or we could use the Perl-side function. It is safe to call this
+ # twice, because the pointer was set to null after it was freed.
+ sbfree($list);
 
 =head1 DESCRIPTION
 
-This C::TinyCompiler package provides Sean Barrett's implementation of stretchy buffers, as
+This C::Blocks package provides Sean Barrett's implementation of stretchy buffers, as
 well as a couple of extensions by David Mertens for popping and removing values
 off the end. For more of Sean Barrett's cool work, see his website at
-http://nothings.org/.
+L<http://nothings.org/>.
 
 How do you begin? Always start by declaring a null pointer of whatever type you
 want, like so:
@@ -163,49 +198,59 @@ of more space on the 'far' end. That is, pushing and popping data off the end is
 easy and relatively fast (though shifting and unshifting off the front is not
 provided).
 
-C::TinyCompiler::StretchyBuffer provides the following interface:
+C::Blocks::StretchyBuffer provides a number of C functions, and a couple
+of Perl functions. Perl functions must be explicitly requested in the
+use statement, i.e. 
+
+ use C::Blocks::StretchyBuffer qw(sbfree);
+
+The C functions are automatically available to any of your C code.
 
 =over
 
 =item sbpush (array, value)
 
-Pushes the given value onto the array, extending it if neccesary. This returns
-the value that was just added to the list.
+C only. Pushes the given value onto the array, extending it if neccesary.
+This returns the value that was just added to the list.
 
 =item sbcount (array)
 
-Returns the number of elements currently available for use.
+C and Perl. Returns the number of elements currently available for use.
+Note that the stretchy buffer may have more room allocated than is
+indicated with this function.
 
 =item sbadd (array, count)
 
-Makes C<count> more elements available, allocating more memory if necessary.
-This returns the address of the new section of memory.
+C only. Makes C<count> more elements available, allocating more memory
+if necessary. This updates C<array> to point to the new address, and
+also returns the address of the new section of memory.
 
 =item sblast (array)
 
-Returns the last available element in the array, an lvalue! Note that this
-assumes that the array is B<not> null, so only call this when you know your
-array is allocated.
+C only. Returns the last available element in the array, an lvalue! Note
+that this assumes that the array is B<not> null, so only call this when
+you know your array is allocated.
 
 =item sbpop (array)
 
-Returns (the value of) the last available element in the array (or zero if the
-array is empty), reducing the array length by 1 (unless the array is empty).
-This does not deallocate the memory, however; that sticks around in case you
-later perform a push or add.
+C only. Returns (the value of) the last available element in the array
+(or zero if the array is empty), reducing the array length by 1 (unless
+the array is empty). This does not deallocate the memory, however; that
+sticks around in case you later perform a push or add.
 
 =item sbremove (array, count)
 
-Removes C<count> elements from the array, or if C<count> is greater than the
-number of elements, empties the array. As with pop, this does not deallocate any
-memory but rather holds onto it in case it can be used for a later push or add.
-As an unplanned but pleasant side-effect, it returns the number of elements that
-remain after the removal.
+C and Perl. Removes C<count> elements from the array, or if C<count> is
+greater than the number of elements, empties the array. As with pop,
+this does not deallocate any memory but rather holds onto it in case it
+can be used for a later push or add. As an unplanned but pleasant
+side-effect, it returns the number of elements that remain after the
+removal.
 
 =item sbfree (array)
 
-Frees the memory associated with the stretchy buffer, if any is allocated. It
-always returns 0.
+C and Perl. Frees the memory associated with the stretchy buffer, if
+any is allocated. It always returns 0.
 
 =back
 
@@ -223,7 +268,7 @@ L<http://github.com/run4flat/perl-TCC/issues>.
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc C::TinyCompiler::StretchyBuffer
+    perldoc C::Blocks::StretchyBuffer
 
 You can also look for information at:
 
@@ -231,20 +276,20 @@ You can also look for information at:
 
 =item * The Github issue tracker (report bugs here)
 
-L<http://github.com/run4flat/perl-TCC/issues>
+L<http://github.com/run4flat/C-Blocks/issues>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
-L<http://annocpan.org/dist/C-TinyCompiler>
+L<http://annocpan.org/dist/C-Blocks>
 
 =item * CPAN Ratings
 
-L<http://cpanratings.perl.org/d/C-TinyCompiler>
+L<http://cpanratings.perl.org/d/C-Blocks>
 
 =item * Search CPAN
 
-L<http://p3rl.org/C::TinyCompiler>
-L<http://search.cpan.org/dist/C-TinyCompiler/>
+L<http://p3rl.org/C::Blocks>
+L<http://search.cpan.org/dist/C-Blocks/>
 
 =back
 
@@ -256,8 +301,8 @@ for putting that code in the public domain!
 =head1 LICENSE AND COPYRIGHT
 
 Sean Barett's original code is in the public domain. All modifications made by
-David Mertens are Copyright 2012 Northwestern University and 2015
-Dickinson College.
+David Mertens are Copyright 2012 Northwestern University, 2015
+Dickinson College, and 2016 Eckerd College.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
